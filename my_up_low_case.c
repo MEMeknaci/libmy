a6c3e8e44ef462b84b7d05e0d3ea6a878c771a0f
Mehdi Meknaci <mehdi.meknaci@epitech.eu>
2020-02-12 08:27:51 -0500

    Added up or lower string

---
+++ my_up_low_case.c
@@ -0,0 +1,33 @@
+/*
+** EPITECH PROJECT, 2019
+** my_strlowcase.c
+** File description:
+** my_strlowcase
+*/
+
+char *my_strupcase(char *str)
+{
+    for (int t = 0; str[t]; t++) {
+        if (str[t] >= 'a' && str[t] <= 'z' )
+            str[t] -= 32;
+    }
+    return (str);
+}
+
+char *my_strlowcase(char *str)
+{
+    for (int t = 0; str[t]; t++) {
+        if (str[t] >= 'A' && str[t] <= 'Z' )
+            str[t] += 32;
+    }
+    return (str);
+}
+
+char *up_or_low_case(char *str, char choice)
+{
+    if (choice == 'u')
+        str = my_strupcase(str);
+    else if (choice == 'l')
+        str = my_strlowcase(str);
+    return (str);
+}
